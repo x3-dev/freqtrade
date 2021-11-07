a66a065ef5263ff6f134bb4fe14649139c00d084
x3-dev <xxx@org.com>
2021-11-07 13:18:51 -0500

    revert

--- freqtrade/rpc/webhook.py
+++ freqtrade/rpc/webhook.py
@@ -65,8 +65,6 @@ class Webhook(RPCHandler):
             payload = {key: value.format(**msg) for (key, value) in valuedict.items()}
             if 'type' not in payload.keys():
                 payload['type'] = msg['type']
-            if 'exchange' not in payload.keys():
-                payload['exchange'] = self._config['exchange'].get('name')
 
             self._send_msg(payload)
         except KeyError as exc:
@@ -74,6 +72,8 @@ class Webhook(RPCHandler):
 
     def _send_msg(self, payload: dict) -> None:
         """ do the actual call to the webhook """
+        if 'exchange' not in payload.keys():
+            payload['exchange'] = self._config['exchange'].get('name')
         try:
             config = dict()
             if self._format == 'form':
